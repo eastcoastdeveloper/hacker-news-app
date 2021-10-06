7c331718cfb84ac91b68eb73f05009ef750d343c
eastcoastdeveloper <uxdeveloper@protonmail.com>
2021-10-06 12:11:33 -0400

    2 files modified

--- Footer.tsx
+++ Footer.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import "./Footer.scss";
+import './Footer.scss';
 
 const Footer = () => {
   return (
     <footer>
-      <span>Hacker News Search App © 2021</span>
-      <a href="https://stackblitz.com/edit/react-ts-iyjbuh" target="_blank">view code</a>
+      <div>
+        <span>Hacker News Search App © 2021</span>
+        <a href="https://stackblitz.com/edit/react-ts-iyjbuh" target="_blank">
+          view code
+        </a>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
