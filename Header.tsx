5fd93a229fd83aad0f0e6bfca0981caf0c36bb10
eastcoastdeveloper <uxdeveloper@protonmail.com>
2022-04-19 09:03:23 -0700

    6 files modified

--- Header.tsx
+++ Header.tsx
@@ -1,36 +1,52 @@
-import React, {useEffect, useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Header.scss';
 
-const Header = (
-  {contentPanel, historyBtn, searchCallback, historyBtnCallback}:
-  {contentPanel: any, historyBtn:any, searchCallback: any, historyBtnCallback:any}) => {
-
-  let searchQuery = "";
+const Header = ({
+  contentPanel,
+  historyBtn,
+  searchCallback,
+  historyBtnCallback,
+}: {
+  contentPanel: any;
+  historyBtn: any;
+  searchCallback: any;
+  historyBtnCallback: any;
+}) => {
+  let searchQuery = '';
 
   const searchField = useRef(null);
 
-  const inputHandler = (event:any) => {
+  const inputHandler = (event: any) => {
     searchQuery = event.target.value;
-  }
+  };
 
   const historyHandler = () => {
-    historyBtnCallback(historyBtn => !historyBtn);
-  }
+    historyBtnCallback((historyBtn) => !historyBtn);
+  };
 
   const clickHandler = () => {
     searchCallback(searchQuery);
-    searchField.current.value = "";
-  }
+    searchField.current.value = '';
+  };
 
   return (
     <React.Fragment>
       <header>
         <div className="title-history">
-          <p onClick={historyHandler}>{contentPanel ? "Hide history" : "Show History"}</p>
+          <p onClick={historyHandler}>
+            {contentPanel ? 'Hide history' : 'Show History'}
+          </p>
         </div>
         <div className="search-field">
-          <input type="text" placeholder="Search tech..." ref={searchField} onChange={inputHandler} />
-          <button type="button" onClick={clickHandler}>GO</button>
+          <input
+            type="text"
+            placeholder="Search tech..."
+            ref={searchField}
+            onChange={inputHandler}
+          />
+          <button type="button" onClick={clickHandler}>
+            GO
+          </button>
         </div>
       </header>
     </React.Fragment>
--- index.tsx
+++ index.tsx
@@ -6,7 +6,6 @@ import Header from './Header';
 import './style.scss';
 
 const App = () => {
-
   let [historyBtn, setHistory] = useState(false);
   let [contentPanel, setPanel] = useState(false);
   let [searchQuery, setState] = useState([]);
@@ -14,51 +13,51 @@ const App = () => {
   const isMounted = useRef(false);
 
   let url = 'https://hn.algolia.com/api/v1/search_by_date?query=',
-      userQuery = 'front_page';
+    userQuery = 'front_page';
 
   const historyBtnCallback = useCallback((historyBtn) => {
     setPanel(historyBtn);
-  }, [])
+  }, []);
 
   const searchCallback = useCallback((val) => {
     userQuery = val;
     isMounted.current = false;
-    !isMounted.current ? fetchData() : "";
+    !isMounted.current ? fetchData() : '';
     setPanel(false);
-  }, [])
+  }, []);
 
   // One fetch for all components
   const fetchData = async () => {
     const res = await fetch(url + userQuery);
     const jsonData = await res.json();
     searchQuery = jsonData.hits;
-    historyList.push(jsonData)
+    historyList.push(jsonData);
     setState(searchQuery);
     setList(historyList);
   };
 
   useEffect(() => {
-    !isMounted.current ? fetchData() : "";
+    !isMounted.current ? fetchData() : '';
     isMounted.current = true;
   }, []);
 
-    return (
-      <div className="wrapper">
-        <Header
-          contentPanel={contentPanel}
-          historyBtn={historyBtn}
-          searchCallback={searchCallback}
-          historyBtnCallback={historyBtnCallback}
-        />
-        <Content
-          contentPanel={contentPanel}
-          searchQuery={searchQuery}
-          historyList={historyList}
-          searchCallback={searchCallback}
-        />
-        <Footer />
-      </div>
-    );
-}
+  return (
+    <div className="wrapper">
+      <Header
+        contentPanel={contentPanel}
+        historyBtn={historyBtn}
+        searchCallback={searchCallback}
+        historyBtnCallback={historyBtnCallback}
+      />
+      <Content
+        contentPanel={contentPanel}
+        searchQuery={searchQuery}
+        historyList={historyList}
+        searchCallback={searchCallback}
+      />
+      <Footer />
+    </div>
+  );
+};
 
 render(<App />, document.getElementById('root'));
